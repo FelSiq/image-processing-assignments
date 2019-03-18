b514f1c31545b5f8a8657ba03c1609ce9c8d0b78
FelSiq <felipe.siqueira@usp.br>
2019-03-18 02:30:28 -0300

    Clink Clonk Idk what I'm doing

--- assignment-01/img_gen.py
+++ assignment-01/img_gen.py
@@ -11,16 +11,14 @@ Student data:
 """
 import matplotlib.pyplot as plt
 import numpy as np
-import imageio
 import random
-import sys
 
 
 GENERATOR_FUNC = {
-    1: lambda x, y, _: x*y + 2*y,
+    1: lambda x, y, _: x*y + 2.0*y,
     2: lambda x, y, Q: abs(np.cos(x/Q) + 2*np.sin(y/Q)),
-    3: lambda x, y, Q: abs(3*(x/Q) - (y/Q)**1/3),
-    4: lambda x, y, _: random.random(),
+    3: lambda x, y, Q: abs(3*(x/Q) - np.power(y/Q, 1/3)),
+    4: lambda x, y, _: np.array([random.uniform(0, 1) for _ in range(len(x))]),
 }
 """."""
 
@@ -34,16 +32,20 @@ def get_parameters():
 
     Raises:
     """
-    args = [str(input()).strip()] + [0] * 6
+    args = [str(input()).rstrip()] + [0] * 6
     
     for i in range(1, 7):
         args[i] = int(input())
+
+    print(args)
 
     return args
 
 
 def RMSE(x, y):
     """Calculate the Root Mean Squared Error from two arrays."""
+    x = x.astype(float)
+    y = y.astype(float)
     return ((x.flatten() - y.flatten())**2.0).sum()**0.5
 
 
@@ -59,35 +61,55 @@ def get_gen_func(f_id):
     return GENERATOR_FUNC.get(f_id)
 
 
-def rescale_image(img, max_val=2**16, min_val=0):
-    """."""
+def rescale_img(img, max_val=2**16-1, min_val=0, dtype=np.uint16):
+    """Something.
+
+    Args:
+
+    Returns:
+
+    Raises:
+    """
     img_min = img.min()
     img_max = img.max()
 
     if img_min == img_max:
         return img
 
+    img = img.astype(float)
+
     return ((max_val - min_val) * (img - img_min)
-            / (img_max - img_min) + min_val)
+            / (img_max - img_min) + min_val).astype(dtype)
 
 
-def randomwalk(C, start_pos=(0, 0), max_steps=100):
-    """."""
+def randomwalk(C, S, start_pos=(0, 0), max_steps=100):
+    """Something.
+
+    Args:
+
+    Returns:
+
+    Raises:
+    """
     x, y = start_pos
     img = np.zeros((C, C))
     img[y, x] = 1
+
+    random.seed(S)
+    np.random.seed(S)
+
     for _ in range(max_steps):
-        dx = 2 * random.random() - 1
-        dy = 2 * random.random() - 1
-    
-        x = round((x + dx)) % C
-        y = round((y + dy)) % C
+        dx = random.uniform(-1, 1)
+        dy = random.uniform(-1, 1)
+
+        x = round(x + dx) % C
+        y = round(y + dy) % C
         img[y, x] = 1
 
-    return rescale_image(img)
+    return rescale_img(img)
 
 
-def generate_image(f_id, C, Q, B):
+def generate_img(f_id, C, Q, S):
     """Something.
 
     Args:
@@ -97,31 +119,102 @@ def generate_image(f_id, C, Q, B):
     Raises:
     """
     if f_id == 5:
-        return randomwalk(C=C, max_steps=1 + C**2)
+        return randomwalk(C=C, max_steps=1 + C**2, S=S)
 
     gen_func = get_gen_func(f_id)
 
     if not gen_func:
         raise ValueError('Invalid generator function id ({})'.format(f_id))
 
+    random.seed(S)
     gen_img = np.array([
-        [gen_func(x, y, Q)
-        for y in range(C)]
+        gen_func(np.repeat(x, C), np.arange(C), Q)
         for x in range(C)
     ])
 
-    return rescale_image(gen_img)
+    return rescale_img(gen_img)
 
-def main():
+def img_downsampling(img, N):
+    """Something.
+
+    Args:
+
+    Returns:
+
+    Raises:
+    """
+    stride = img.shape[0] // N
+
+    img_downsampled = np.array([
+      [img[j * stride, i * stride]
+      for i in range(N)]
+      for j in range(N)
+    ])
+
+    return img_downsampled
+
+def img_quantisation(img, B):
+    """Something.
+
+    Args:
+
+    Returns:
+
+    Raises:
+    """
+    img = rescale_img(
+        img,
+        max_val=2**8-1,
+        min_val=0,
+        dtype=np.uint8)
+
+    img >>= (8-B)
+
+    return img
+
+def main(subpath=None, plot=False):
+    """Something.
+
+    Args:
+
+    Returns:
+
+    Raises:
+    """
     filename, C, f_id, Q, N, B, S = get_parameters()
 
-    random.seed(S)
+    gen_img = generate_img(f_id, C, Q, S)
 
-    gen_img = generate_image(f_id, C, Q, B)
+    gen_img = img_downsampling(gen_img, N) 
 
-    plt.imshow(gen_img, cmap="gray")
-    plt.show()
+    gen_img = img_quantisation(gen_img, B)
+
+    if subpath:
+        filename = "/".join((subpath, filename))
+
+    ref_img = np.load(filename).astype(np.uint8)
+
+    if plot:
+        plt.subplot(131)
+        plt.title("Generated (min={:.4f}"
+                  "max={:.4f})".format(gen_img.min(), gen_img.max()))
+        plt.imshow(gen_img, cmap="gray")
+
+        plt.subplot(132)
+        plt.title("Reference (min={:.4f}"
+                  "max={:.4f})".format(ref_img.min(), ref_img.max()))
+        plt.imshow(ref_img, cmap="gray")
+
+        plt.subplot(133)
+        plt.title("Difference")
+        plt.imshow(abs(ref_img.astype(float) - gen_img.astype(float)), cmap="gray")
+
+        plt.show()
+
+    rmse = RMSE(gen_img, ref_img)
+
+    print("{:.4f}".format(rmse))
 
 
 if __name__ == "__main__":
-    main()
+    main(subpath="./documents", plot=True)
