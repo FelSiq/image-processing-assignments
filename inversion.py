4b367552bea4154127eff352507f37b40738ca90
FelSiq <felipe.siqueira@usp.br>
2019-03-23 15:13:31 -0300

    Added image inversion code

---
+++ inversion.py
@@ -0,0 +1,33 @@
+import image_base
+
+
+class ImageInverter(image_base.ImageManipulatorBase):
+    def __init__(self, img):
+        """Invert image pixel values using a simple formulation.
+
+        pixel'[i, j] = (img_max + img_min) - pixel[i, j]
+        """
+        super().__init__(img)
+
+    def transform_img(self):
+        """."""
+        if self.img is None:
+            raise TypeError("Please fit an image in model first.")
+
+        img_range = self.img.max() + self.img.min()
+
+        self.img_mod = img_range - self.img
+
+        return self.img_mod
+
+
+if __name__ == "__main__":
+    import imageio
+    md1 = ImageInverter(imageio.imread("./images/nap.jpg"))
+    md2 = ImageInverter(imageio.imread("./images/scarlett.jpg"))
+
+    md1.transform_img()
+    md2.transform_img()
+
+    md1.plot()
+    md2.plot()
